6c44958fb993feec83d3ec65810240e365b2547a
Suphal Maity <suphalmaity730@gmail.com>
2024-10-11 13:07:28 +0530

    Update index.js

--- ConnectMongo/index.js
+++ ConnectMongo/index.js
@@ -1,1 +1,34 @@
+const express = require('express')
+const mongoose=require('mongoose')
+
+
+// suphal_practice
+// YC1H3ToYVtDvtoa6
+
+async function connectDB(){
+  
+  try{
+    const connectData= await mongoose.connect('mongodb+srv://suphal_practice:YC1H3ToYVtDvtoa6@cluster0.9vfrosu.mongodb.net/Todo_app');
+    console.log('connected to database');
+    console.log("host:",connectData.connection.host)
+    app.listen(3000,()=>{
+      console.log("server started")
+    })
+    
+  }catch(err){
+    console.error(err)
+  } 
+}
+
+connectDB();
+
+
+
+
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
 
